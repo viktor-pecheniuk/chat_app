1e6ecc151eff9e01b6bad68c11be7d24b4ae9216
viktor <viktor.pecheniuk@gmail.com>
2016-11-08 17:02:04 +0200

    done step 2

--- imports/ui/Conversations.jsx
+++ imports/ui/Conversations.jsx
@@ -1,70 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+// API
+import { Chats } from '/imports/api/chats.js';
 import Moment from 'moment';
 
 import Chat from './Chat.jsx';
 
-export default class Chats extends Component {
-  constructor(props) {
-    super(props);
+export default class Conversations extends Component {
 
-    this.state = {
-      chats: [
-        {
-          _id: 0,
-          name: 'Ethan Gonzalez',
-          picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
-          lastMessage: {
-            text: 'You on your way?',
-            timestamp: Moment().subtract(1, 'hours').toDate()
-          }
-        },
-        {
-          _id: 1,
-          name: 'Bryan Wallace',
-          picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg',
-          lastMessage: {
-            text: 'Hey, it\'s me',
-            timestamp: Moment().subtract(2, 'hours').toDate()
-          }
-        },
-        {
-          _id: 2,
-          name: 'Avery Stewart',
-          picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg',
-          lastMessage: {
-            text: 'I should buy a boat',
-            timestamp: Moment().subtract(1, 'days').toDate()
-          }
-        },
-        {
-          _id: 3,
-          name: 'Katie Peterson',
-          picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg',
-          lastMessage: {
-            text: 'Look at my mukluks!',
-            timestamp: Moment().subtract(4, 'days').toDate()
-          }
-        },
-        {
-          _id: 4,
-          name: 'Ray Edwards',
-          picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg',
-          lastMessage: {
-            text: 'This is wicked good ice cream.',
-            timestamp: Moment().subtract(2, 'weeks').toDate()
-          }
-        }
-      ]
-    }
-  }
-
+  deleteChat(chat) {
+  Chats.remove(chat._id);
+}
   renderChats() {
-    return this.state.chats.map((chat) => (
-      <Chat
-        key={chat._id}
-        chat={chat}/>
-    ));
-  }
+  return this.props.chats.map((chat) => (
+    <Chat
+      key={chat._id}
+      chat={chat}
+      deleteChat={this.deleteChat.bind(this)} />
+  ));
+}
 
   render() {
     return (
@@ -74,3 +28,13 @@ export default class Chats extends Component {
     );
   }
 }
+
+Conversations.propTypes = {
+  chats: PropTypes.array.isRequired,
+};
+
+export default createContainer(() => {
+  return {
+    chats: Chats.find({}).fetch(),
+  };
+}, Conversations);
